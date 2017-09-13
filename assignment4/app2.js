d77c56225bd513fa37f8c2ba9ee1ad67bbfb66c6
Samarth Gupta <sgupta@grepruby.com>
2017-09-13 15:34:04 +0530

    Made changes as suggested in node app using 3rd party API

--- assignment4/app2.js
+++ assignment4/app2.js
@@ -1,113 +1,106 @@
-var express = require("express");
-var request = require("request");
-var app = express();
-var bodyParser = require('body-parser').json();
-
-var AllClientOptions = {
-  url: "https://api.createsend.com/api/v3.1/clients.json?pretty=true",
-  auth: {
-    username: "9a86a35217185b745506196eb27709a0",
-    password: "x"
-  }
-};
-
-var CreateClientOptions = {
-  url: "https://api.createsend.com/api/v3.1/clients.json",
-  auth: {
-    username: "9a86a35217185b745506196eb27709a0",
-    password: "x"
-  }
-};
+const express = require("express");
+const request = require("request");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.listen(3000, function() {
   console.log("The Node Server is running on port 3000");
 });
 
-app.get("/", function(req, res) {
-  request.get(AllClientOptions, function(error, resp, body) {
-    if (error) console.log("Error");
-    res.send(body);
-  });
+app.get("/clients", function(req, res) {
+  request.get(
+    {
+      url: "https://api.createsend.com/api/v3.1/clients.json?pretty=true",
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    },
+    function(error, resp, body) {
+      if (error) throw error;
+      res.status(200).send(body);
+    }
+  );
 });
 
-app.get("/getclients/:id", function(req, res) {
-  var userid = req.params.id;
+app.get("/clients/:id", function(req, res) {
+  var userId = req.params.id;
   var url =
     "https://api.createsend.com/api/v3.1/clients/" +
-    userid.toString() +
+    userId.toString() +
     ".json";
   request.get(
     {
       url: url,
-      auth: {
-        username: "9a86a35217185b745506196eb27709a0",
-        password: "x",
-        sendImmediately: false
+      headers: {
+        Authorization: req.headers.authorization
       }
     },
-    function(error, resp, body) {
-      if (error) return console.log("Error");
-      res.send(body);
+    function(err, resp, body) {
+      if (err) throw err;
+      res.status(200).send(body);
     }
   );
 });
 
-app.get("/listClient/:list", function(req, res) {
-  var userid = req.params.list;
+app.get("/clients/:id", function(req, res) {
+  var userId = req.params.id;
   var url =
     "https://api.createsend.com/api/v3.1/clients/" +
-    userid.toString() +
+    userId.toString() +
     "/lists.json";
-  request.get(
+  request.put(
     {
       url: url,
-      auth: {
-        username: "9a86a35217185b745506196eb27709a0",
-        password: "x",
-        sendImmediately: false
-      }
+      headers: {
+        Authorization: req.headers.authorization
+      },
+      body: req.body
     },
     function(error, resp, body) {
-      if (error) return console.log("Error");
-      res.send(body);
+      if (error) throw err;
+      res.status(200).send(body);
     }
   );
 });
 
-app.get("/listClient/:user", function(req, res) {
-  var usertodelete = req.params.user;
+app.get("/clients/:user", function(req, res) {
+  var idToDelete = req.params.user;
   var url =
     "https://api.createsend.com/api/v3.1/clients/" +
-    usertodelete.toString() +
+    idToDelete.toString() +
     ".json";
   request.delete(
     {
       url: url,
-      auth: {
-        username: "9a86a35217185b745506196eb27709a0",
-        password: "x",
-        sendImmediately: false
+      headers: {
+        Authorization: req.headers.authorization
       }
     },
     function(error, resp, body) {
-      if (error) return console.log("Error");
-      res.send(body);
+      if (error) throw error
+      res.status(200).send(body);
     }
   );
 });
 
-app.post('/createClient', bodyParser, function(req, res) {
-  request.post({
-	  url: "https://api.createsend.com/api/v3.1/clients.json",
-	  json: true,
-	  auth: {
-	    username: "9a86a35217185b745506196eb27709a0",
-	    password: "x"
-	  },
-	    body: req.body
-	},
-	    function(error, resp, body) {
-    	  if(error) console.log(error);
-    	  res.send(body);
-    });  
+app.post("/clients", function(req, res) {
+  request.post(
+    {
+      url: "https://api.createsend.com/api/v3.1/clients.json",
+      json: true,
+      headers: {
+        Authorization: req.headers.authorization
+      },
+      body: req.body
+    },
+    function(error, resp, body) {
+      if (error) throw error;
+      res.status(200).send(body);
+    }
+  );
 });
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + 'Not Found'});
+})
